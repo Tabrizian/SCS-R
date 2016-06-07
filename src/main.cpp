1aea5f0239f2d2ffe885629f1098bcfae366d8f1
Iman Tabrizian <tabrizian@outlook.com>
2016-06-07 15:38:33 +0430

    Finish getting input.

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,16 @@ int main(int argc, char** argv) {
         if (argv[0][0] == 'f') {
         }
     } else {
+        int num_of_strings;
+        cin >> num_of_strings;
+
+        char** strs = new char*[num_of_strings];
+
+        for(int i = 0; i < num_of_strings; i++) {
+            cin >> strs[i];
+        }
+
+        cout << greedy_r(strs, num_of_strings) << endl;
     }
     return 0;
 }
