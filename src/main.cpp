a7e66bc2958da5aff12a265520319d3f945b1ca8
Iman Tabrizian <tabrizian@outlook.com>
2016-06-07 05:41:42 +0430

    Adds functions prototype

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    if(argc != 0) {
+        if (argv[0][0] == 'f') {
+        }
+    } else {
+    }
+    return 0;
+}
---
+++ src/scs-r.h
@@ -0,0 +1,8 @@
+#ifndef SCS-R_H
+#define SCS-R_H
+
+int overlap(char *str1, char *str2);
+bool factor(char *source, char *factor);
+bool make_reverse_factor_free(char **strs, int size);
+
+#endif
